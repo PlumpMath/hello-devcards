d433b087406d648f2a39e99f1b0968a14d17a257
Walter P Babic <wbabic@mac.com>
2016-02-17 07:01:31 -0700

    updated pages

--- pages-src/pages/core.cljs
+++ pages-src/pages/core.cljs
@@ -4,7 +4,8 @@
    [complex.number :as n :refer [zero one i negative-one negative-i infinity add sub mult div]]
    [hello-devcards.bezier]
    [hello-devcards.pixie-turtle]
-   [hello-devcards.chessboard])
+   [hello-devcards.chessboard]
+   [hello-devcards.color-wheel])
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest defcard-rg defcard-doc]]))
 
