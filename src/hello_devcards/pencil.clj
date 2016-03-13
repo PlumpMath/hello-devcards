da5674de67330dad153b3af79d2283cb8b2b5f2c
Walter P Babic <wbabic@mac.com>
2016-03-12 17:01:37 -0700

    added ticker-chan

--- src/hello_devcards/pencil.clj
+++ src/hello_devcards/pencil.clj
@@ -35,5 +35,67 @@
 ;; once around in 60 ticks
 ;; pub sub
 
+(def ticker-chan (async/chan 1))
+
+(def ticker-mult (async/mult ticker-chan))
+
+(defn send-n-ticks [n ch]
+  (async/go-loop [i 0]
+    (println "waiting ....")
+    (async/<! (async/timeout 1000))
+    (println (str "sending a system tick: " i))
+    (async/>! ch [:tick i])
+    (println "tick sent")
+    (if (< i n)
+      (recur (inc i)))))
+
+(defn process-ticks [ch]
+  (async/go
+    (loop []
+      (if-let [t (async/<! ch)]
+        (do (println t)
+            (recur))
+        (println :done)))))
+
+(comment
+  (let [c (async/chan 1)]
+    (async/tap ticker-mult c)
+    (process-ticks c)
+    (async/untap ticker-mult c))
+
+  (send-n-ticks 10 ticker-chan)
+
+  )
+
 (defn sqawn-linear-turtle [inital-turtle ]
+  )
+
+;; parameterized semi circle
+(defn p [n]
+  (let [d (+ (* n n) 1)]
+    [(/ (* 2 n) d) (/ (- (* n n) 1) d)]))
+
+(defn sq-sum [[x y]]
+  (+ (* x x) (* y y)))
+
+(comment
+  (for [n (range 1 11)]
+    [n (p n) (p (/ n)) (sq-sum (p n)) (sq-sum (p (/ n)))])
+
+  (for [n (range 2 101)]
+    (p n))
+  )
+
+(defn trip [n]
+  [(* 2 n) (- (* n n) 1) (+ (* n n) 1)])
+
+(defn py-trip? [a b c]
+  (= (+ (* a a) (* b b)) (* c c)))
+
+(comment
+  (map trip (range 1 11))
+  ;;=> ([2 0 2] [4 3 5] [6 8 10] [8 15 17] [10 24 26] [12 35 37] [14 48 50] [16 63 65] [18 80 82] [20 99 101])
+  (map #(apply py-trip? %) (map trip (range 1 11)))
+  (every? true? (map #(apply py-trip? %) (map trip (range 1 11))))
+  ;;=> true
   )
