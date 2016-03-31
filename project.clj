a7b696e07054169b260e58ea5a7b4bb9f913cc45
Walter P Babic <wbabic@mac.com>
2016-03-30 21:25:52 -0600

    update dependencies

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.8.34"]
-                 [org.clojure/tools.reader "1.0.0-alpha2"]
+                 [org.clojure/clojurescript "1.8.40"]
+                 [org.clojure/tools.reader "1.0.0-alpha3"]
                  [org.clojure/tools.analyzer.jvm "0.6.9"]
 
                  [devcards "0.2.1-6" :exclusions [org.clojure/tools.reader]]
-                 [sablono "0.6.2"]
+                 [sablono "0.6.3"]
                  [reagent "0.6.0-alpha"]
 
                  [org.clojure/core.match "0.3.0-alpha4"]
@@ -24,8 +24,8 @@
                  [fipp "0.6.4"]
                  ]
 
-  :plugins [[lein-cljsbuild "1.1.2"]
-            [lein-figwheel "0.5.0-6"
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.2"
              :exclusions [org.clojure/clojure
                           ring/ring-core joda-time
                           org.clojure/tools.reader]]]
